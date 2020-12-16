a1142a6adde75f079d4a34aa3f9a074f7d68868a
Vladyslav Horbachov <0681813464.dev@gmail.com>
2020-12-16 14:55:52 +0200

    Added a few ways how to delete folders and files

--- clean.ps1
+++ clean.ps1
@@ -1,1 +1,14 @@
-Write-Host "Congratulations! Your first script executed successfully"
+# Way 1
+# Remove-Item -Path .\my -Force -Recurse
+
+# Way 2
+# $ProjectItems = 'Controllers','Robots','Weapons','Bacon'
+
+# $DTE.Solution.Projects|
+# Select-Object -Expand ProjectItems|
+# Where-Object{$ProjectItems -contains $_.Name}|
+# ForEach-Object{$_.Remove()}
+
+#Way 3
+# $folders = @("~/MyFolder/Test123", "~/MyFolder/Test987", "~/MyFolder/Test333")
+# $folders | Remove-Item 
